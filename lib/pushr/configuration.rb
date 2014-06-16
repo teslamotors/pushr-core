bd636432c4c127f789893cdbfd8e7d4642d2cdb9
Tilo Sloboda <tsloboda@teslamotors.com>
2014-06-16 09:35:19 -0700

    fixing location of configuration_file

--- lib/pushr/configuration.rb
+++ lib/pushr/configuration.rb
@@ -30,7 +30,7 @@ module Pushr
     end
 
     def self.all
-      if Pushr::Daemon.config.configuration_file # only set if file exists
+      if Pushr::Core.configuration_file # only set if file exists
         read_from_yaml_file
       else
         read_from_redis
@@ -49,7 +49,7 @@ module Pushr
     private
 
     def read_from_yaml_file
-      filename = Pushr::Daemon.config.configuration_file
+      filename = Pushr::Core.configuration_file
       configs = File.open(filename) { |fd| YAML.load(fd) }
       configs.map do |hsh|
         klass = hsh['type'].split('::').reduce(Object) { |a, e| a.const_get e }
